d6b30372ffb94f1f1fa2af07e36fd1951239bf02
Matthew <matthew6086888@hotmail.com>
2021-07-29 18:15:01 -0600

    Landing, Header, Fonts, are now added

---
+++ app.js
@@ -0,0 +1,2 @@
+// imports all javascript code. Included in every html file
+import layout from "./js/components.js";
---
+++ js/components.js
@@ -0,0 +1,29 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `<header>
+        <nav>
+            <ul>
+                <li class="desktop"><a href="./index.html"><img src="./images/Logo.jpg" alt=""></a></li>
+                <li><a href="./index.html">Home</a></li>
+                <li><a href="./html/shop.html">Shop</a></li>
+                <li><a href="./html/about.html">About</a></li>
+                <li><a href="./html/contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>`;
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+customElements.define('header-component', Header);
+
+const all = {
+    Header,
+}
+export default all;
