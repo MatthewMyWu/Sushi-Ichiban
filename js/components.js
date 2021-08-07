87621029206df1cc2a2f337ddf5ea1b197295def
Matthew <matthew6086888@hotmail.com>
2021-08-06 19:20:43 -0600

    Basic reusable menu items

--- js/components.js
+++ js/components.js
@@ -17,13 +17,32 @@ class Header extends HTMLElement {
     </header>`;
     }
 }
+
+
+
+class MenuItem extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `<div class="menu-item">
+            <h3 class="dish-name">Dish Name</h3>
+            <p class="dish-description">Rare sliced beef with ponzu sauce</p>
+            <p class="dish-price">7.50</p>
+            <img src="./images/Sample Sushi.jpg" alt="">
+        </div>`;
+    }
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 customElements.define('header-component', Header);
+customElements.define('menu-item-component', MenuItem);
 
 const all = {
     Header,
+    MenuItem,
 }
 export default all;
