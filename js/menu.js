a389ca2ce89934cd95f8ae86569b31df2985d40b
Matthew <matthew6086888@hotmail.com>
2021-08-07 17:09:11 -0600

    Added reusable menu-item, menu-section components

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
-const MenuItem = Vue.component('menu-item', {
-    props: ['name', 'description', 'price'],
+const MenuItem = Vue.component("menu-item", {
+    props: ["name", "description", "price"],
     template: `<div class="menu-item">
     <h3 class="dish-name">{{name}}</h3>
     <p class="dish-description">{{description}}</p>
@@ -7,29 +7,83 @@ const MenuItem = Vue.component('menu-item', {
     <img class="dish-image" src="./images/Sample Sushi.jpg" alt="">
 </div>`
 });
-// <img src={{image}} alt="">
+
+const MenuSection = Vue.component("menu-section", {
+    props: ["section_name"],
+    template: `
+    <section class="menu-section">
+        <h2 class="menu-section-title">{{section_name.name}}</h2>
+                <div class="items-container">
+                    <menu-item v-for="menuItem in ` + "section_name" + `.items" v-bind:name="menuItem.name" v-bind:description="menuItem.description"
+                        v-bind:price="menuItem.price" v-bind:key="menuItem.id"></menu-item>
+                </div>
+            </section>
+    `
+})
+
+
+
+const soupsAndSalads = {
+    name: "Soups and Salads",
+    items: [
+        {
+            name: "soup and salad 1",
+            description: "sas 1",
+            price: "1",
+        },
+        {
+            name: "soup and salad 2",
+            description: "sas 2",
+            price: "2",
+        },
+        {
+            name: "soup and salad 3",
+            description: "sas 3",
+            price: "3",
+        },
+        {
+            name: "soup and salad 4",
+            description: "sas 4",
+            price: "4",
+        },
+    ]
+};
+
+const seafood = {
+    name: "Seafood",
+    items: [
+        {
+            name: "seafood 1",
+            description: "sea 1",
+            price: "1",
+        },
+        {
+            name: "seafood 2",
+            description: "sea 2",
+            price: "2",
+        },
+        {
+            name: "seafood 3",
+            description: "sea 3",
+            price: "3",
+        },
+        {
+            name: "seafood 4",
+            description: "sea 4",
+            price: "4",
+        },
+    ]
+};
+
+
+
 
 
 
 const menuApp = new Vue({
     el: "#menu-app",
     data: {
-        menuItems: [
-            {
-                name: "Item 1",
-                description: "Description 1",
-                price: "1",
-            },
-            {
-                name: "Item 2",
-                description: "Description 2",
-                price: "2",
-            },
-            {
-                name: "Item 2",
-                description: "Description 2",
-                price: "2",
-            }
-        ]
+        soupsAndSalads: soupsAndSalads,
+        seafood: seafood,
     }
 });
