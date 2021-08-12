c5c03d8c98718b2aa0cd6792c1257c9c8ec24cba
Matthew <matthew6086888@hotmail.com>
2021-08-12 16:07:46 -0600

    Potential header bug fix

--- js/components.js
+++ js/components.js
@@ -8,7 +8,7 @@ class Header extends HTMLElement {
         <nav>
             <ul>
                 <li class="desktop"><a href="./index.html"><img src="./images/Logo.jpg" alt=""></a></li>
-                <li><a href="./index.html">Home</a></li>
+                <li><a href="./index.html">Home1</a></li>
                 <li><a href="./html/menu.html">Menu</a></li>
                 <li><a href="./html/contact.html">Contact</a></li>
             </ul>
