92f70cc9da35aa810ad78b84ed61e057f5727836
MatthewMyWu <matthew6086888@hotmail.com>
2025-09-30 11:22:50 -0600

    Update build config again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from "@vitejs/plugin-vue";
 import {viteStaticCopy} from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
+import { resolve } from 'path';
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,4 +16,13 @@ export default defineConfig({
       ],
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        menu: resolve(__dirname, 'menu.html'),
+      }
+    }
+  }
 });
